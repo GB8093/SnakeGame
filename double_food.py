ab4722faf576270706869e5e6e5dc83b2e9c72e8
GB8093 <148652921+GB8093@users.noreply.github.com>
2024-09-02 16:54:20 +0530

    Initial commit

---
+++ double_food.py
@@ -0,0 +1,21 @@
+import random
+from turtle import Turtle
+import time
+
+
+class Bfood(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=1, stretch_wid=1)
+        self.color("yellow")
+        self.speed("slowest")
+        self.refresh()
+
+
+    def refresh(self):
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_y)
